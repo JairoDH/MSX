8ae4de6252c4479ab1ea27ce68b3e130f525b76f
JairoDH <jairodominguez3594@gmail.com>
2023-05-03 19:02:26 +0200

    errores en /juegos

--- app.py
+++ app.py
@@ -14,30 +14,33 @@ def inicio():
 
 #definición de función de vista (GET, cuando se accede a la página y POST, el usuario envía información por el formulario)
 #si el usuario introduce información se extrae el valor del campo entrada nombre y redirige a la página de resultados de búsqueda
-#usando la función redirect()(para redireccionar a la página) y url_for().
+#usando la función render_template()(para redireccionar a la página) y url_for().
 #si la solicitud es GET, se ejecuta el else devolviendo el contenido HTML (/juegos.html)
 
-@app.route('/juegos', methods = ['GET', 'POST'])
+@app.route('/juegos', methods = ["GET", "POST"])
 def juegos():
     if request.method == 'POST':
-        nombre = request.form['nombre']
-        return redirect(url_for('listajuegos', nombre = nombre))
+        nombre = request.form.get["nombre"]
+        return render_template("listajuegos.html", nombre = nombre)
     else:
-        return render_template("juegos.html")
+        return render_template("juegos.html", datos = datos)
 
 #funcion de vista POST (introducido anteriormente por el usuario), se extrae el valor de entrada nombre del formulario
 #y se realiza la búsqueda en la lista de la variable datos(variable donde esta el json.MSX)
 #cuyo nombre comience por el valor del campo nombre(.startswith()) y lo añade a la lista resultados
 #devuelve (return) la plantilla HTML(listajuegos) y pasa la lista resultados.
 
-@app.route('/listajuegos', methods =['POST'])
+@app.route('/listajuegos', methods =["POST"])
 def listajuegos():
-    nombre = request.form['nombre']
+    nombre = str(request.form['nombre'])
     resultado = []
     for juego in datos:
-        if juego['nombre'].startswith(nombre):
+        if str(juego['nombre']).lower().startswith(str(nombre).lower()):
             resultado.append(juego)
-    return render_template('listajuegos.html',resultado = resultado)
+    else:
+        return render_template("listajuegos.html",resultado = resultado)
+    
+
 
 
 app.run("0.0.0.0",5000,debug=True)
